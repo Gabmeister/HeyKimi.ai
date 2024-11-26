4357f2ce2a9f77d9b36cc7bb92bce81a57d3ea28
Gabriel Plaza <plaza32007@gmail.com>
2024-11-26 20:25:01 +0000

    added draft logo by Daavid

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import '../pages/LandingPage.css';
+import kimilogo from '../components/kimilogo.png';
 
 const LandingPage = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div className="landing-page">
+      <img 
+        src={kimilogo} 
+        alt="Kimi Logo" 
+        className="landing-page-logo" 
+      />
+      <h1>Welcome to HeyKimi.ai</h1>
+      <p>Kimi is your new state-of-the-art AI Sales Agent</p>
+    </div>
+  );
 };
 
 export default LandingPage;

5852ef0c6e61d1cb06d71d54dd0b1b180a80bedb
Gabriel Plaza <plaza32007@gmail.com>
2024-11-26 22:37:08 +0000

    logo revised

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <nav>
-      <ul>
-        <li><Link to="/">LandingPage</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
+const Navbar = () =>{
+  return(
+    <nav className="bg-purple-900 text-white p-4" style={{ backgroundColor: "#0E0915" }}>
+      <ul className="flex space-x-6">
+        <li>
+          <Link
+            to="/"
+            className="hover:text-purple-400 transition duration-300"
+          >
+            LandingPage
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/login"
+            className="hover:text-purple-400 transition duration-300"
+          >
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/dashboard"
+            className="hover:text-purple-400 transition duration-300"
+          >
+            Dashboard
+          </Link>
+        </li>
       </ul>
     </nav>
   );
